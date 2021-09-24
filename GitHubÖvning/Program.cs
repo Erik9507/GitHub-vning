6969481f488bf7b9860303727d725ff44ef00a0f
Erik9507 <eriknorell1995@gmail.com>
2021-09-24 10:50:44 +0200

    Add project files.

---
+++ GitHubÖvning/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GitHubÖvning
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
